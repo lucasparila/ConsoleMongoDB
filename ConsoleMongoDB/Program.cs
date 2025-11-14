516d9ab89a12fe3064293a4e2ba6758cafcc6b43
Lucas Parila <lucas.eparila@gmail.com>
2025-11-14 14:25:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleMongoDB/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleMongoDB;
+using MongoDB.Driver;
+
+
+var cliente = new MongoClient("mongodb+srv://lucaseparila_db_user:5tBRs36wUj3jQmKt@interacaomongo.vumklsf.mongodb.net/"); // conexão com o atlas
+//var cliente = new MongoClient("mongodb:// localhost:27017"); // para conectar no docker
+
+var database = cliente.GetDatabase("InterAcaoMongoDb"); // use nomedobanco
+
+var collection = database.GetCollection<User>("Usuarios");
+
+// var usuario = new User("Felipe", "123mudar");
+
+//collection.InsertOne(usuario);
+
+//var usuariosNovos = new List<User>();
+//usuariosNovos.Add(new User("Maria", "123mudar"));
+//usuariosNovos.Add(new User("Luiz", "123mudar"));
+//usuariosNovos.Add(new User("Ana", "123mudar"));
+//collection.InsertMany(usuariosNovos);
+
+//var usuarios = collection.Find(x => true).ToList();
+
+//foreach(var usuar in usuarios)
+//{
+//   Console.WriteLine(usuar);
+//   Console.WriteLine("---");
+//}
+
+//var usuario = collection.Find(x => x.Login == "Felipe").FirstOrDefault(); // traz a primeira ocorrência encontrada
+//var usuario = collection.Find(x => x.Login == "Felipe").ToList(); // traz todas as ocorrências 
+//Console.WriteLine(usuario);
+
+//collection.UpdateOne<User>(x => x.Id == "691739cdef79586e8b3f68bf",)
+
+
+//var usuario = collection.Find(x => x.Id == "691739cdef79586e8b3f68bf").FirstOrDefault();
+
+//Console.WriteLine("Cgegou do banco assim: \n" + usuario);
+
+//Console.WriteLine("########");
+//usuario.Password = "456mudar"; // atualização em memoria na aplicação 
+
+//collection.ReplaceOne(x => x.Id == usuario.Id, usuario); // salva no banco a atualização
+//var usuario1 = collection.Find(x => x.Id == "691739cdef79586e8b3f68bf").FirstOrDefault();
+//Console.WriteLine(usuario1);
+
+var usuario = collection.Find(x => x.Id == "691739cdef79586e8b3f68c0").FirstOrDefault();
+
+Console.WriteLine("Cgegou do banco assim: \n" + usuario);
+
+//var filtro = Builders<User>.Filter.Eq(u => u.Id, "691739cdef79586e8b3f68c0");
+
+//var update = Builders<User>.Update.Set(u => u.Password, "159mudar");
+
+//collection.UpdateOne(filtro, update);
+
+
+//collection.UpdateOne(
+//    x => x.Id == "691739cdef79586e8b3f68c0",
+//    Builders<User>.Update.Set(x => x.Password, "100mudar")    atualiza um único campo do objeto
+//);
+
+collection.UpdateOne(
+    x => x.Id == "691739cdef79586e8b3f68c0", 
+    Builders<User>.Update.Set(x => x.Password, "100mudar").Set(x => x.Login, "Lucas") // atualiza dois campos do objeto 
+);
+
+
+usuario = collection.Find(x => x.Id == "691739cdef79586e8b3f68c0").FirstOrDefault();
+
+Console.WriteLine( usuario);
---
+++ ConsoleMongoDB/User.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMongoDB
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreateAt { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime UpdateAt { get; set; }
+        public bool IsActive { get; set; }
+
+        public User(string login, string password)
+        {
+            
+            Login = login;
+            Password = password;
+            CreateAt = DateTime.Now;
+            UpdateAt = DateTime.Now;
+            IsActive = true;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}\n" +
+                $"Login: {this.Login}\n" +
+                $"Senha: {this.Password}\n" +
+                $"Data de Criação: {this.CreateAt}\n" +
+                $"Ultima atualização: {this.UpdateAt}\n" +
+                $"Ativo: {this.IsActive}";
+        }
+    }
+}
